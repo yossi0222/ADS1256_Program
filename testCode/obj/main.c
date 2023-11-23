736ccc5bca960baa47fa307b465e6ce5a65b12ce
yossi0222 <keigo.0804@icloud.com>
2023-11-23 15:40:33 +0900

    リスト保存

--- testCode/obj/main.c
+++ testCode/obj/main.c
@@ -7,9 +7,11 @@
 #include <string.h>
 #include <sys/timeb.h>
 
+#define SAMPLE_COUNT 500  // データを保存するサンプル数
+
 void Handler(int signo)
 {
-    //System Exit
+    // System Exit
     printf("\r\nEND                  \r\n");
     DEV_ModuleExit();
 
@@ -40,17 +42,28 @@ int main(void)
     clock_gettime(CLOCK_MONOTONIC, &start_time);
     clock_gettime(CLOCK_MONOTONIC, &current_time);
 
+    float data_buffer[SAMPLE_COUNT]; // データを一時的に保存するバッファ
+    int data_index = 0;
+
     while ((current_time.tv_sec - start_time.tv_sec) < 5) // 5秒間ループする
     {
         clock_gettime(CLOCK_MONOTONIC, &elapsed_time);
-        printf("Time: %ld seconds, %ld nanoseconds | Value: %f\n",
-               elapsed_time.tv_sec - start_time.tv_sec,
-               elapsed_time.tv_nsec - start_time.tv_nsec,
-               ADS1256_GetChannalValue(1) * 5.0 / 0x7fffff);
+
+        if (data_index < SAMPLE_COUNT)
+        {
+            data_buffer[data_index] = ADS1256_GetChannalValue(1) * 5.0 / 0x7fffff;
+            data_index++;
+        }
 
         nanosleep(&delay_time, NULL); // 10マイクロ秒待つ
 
         clock_gettime(CLOCK_MONOTONIC, &current_time);
+    }
+
+    // データを一気に出力
+    for (int i = 0; i < data_index; i++)
+    {
+        printf("Data %d: %f\n", i + 1, data_buffer[i]);
     }
 
     return 0;
