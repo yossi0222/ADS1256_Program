8f71082671663540655634202513e79e061cbfff
yossi0222 <keigo.0804@icloud.com>
2023-11-23 15:35:45 +0900

    時間経過測定

--- testCode/obj/main.c
+++ testCode/obj/main.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>     
-#include <signal.h>     
+#include <stdlib.h>
+#include <signal.h>
 #include <time.h>
 #include "ADS1256.h"
 #include "stdio.h"
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <sys/timeb.h>
 
-void  Handler(int signo)
+void Handler(int signo)
 {
     //System Exit
     printf("\r\nEND                  \r\n");
@@ -21,29 +21,34 @@ int main(void)
     UDOUBLE ADC[8], i;
     printf("demo\r\n");
     DEV_ModuleInit();
-    
+
     // Exception handling: ctrl + c
     signal(SIGINT, Handler);
 
-    if (ADS1256_init() == 1){
+    if (ADS1256_init() == 1)
+    {
         printf("\r\nEND                  \r\n");
         DEV_ModuleExit();
         exit(0);
     }
 
     struct timespec delay_time;
-    delay_time.tv_sec = 0;  // 10マイクロ秒 = 0秒
+    delay_time.tv_sec = 0;     // 10マイクロ秒 = 0秒
     delay_time.tv_nsec = 10000; // ナノ秒単位で10マイクロ秒 = 10000ナノ秒
 
-    struct timespec start_time, current_time;
+    struct timespec start_time, current_time, elapsed_time;
     clock_gettime(CLOCK_MONOTONIC, &start_time);
     clock_gettime(CLOCK_MONOTONIC, &current_time);
 
-    while ((current_time.tv_sec - start_time.tv_sec) < 5) {  // 5秒間ループする
+    while ((current_time.tv_sec - start_time.tv_sec) < 5) // 5秒間ループする
+    {
+        clock_gettime(CLOCK_MONOTONIC, &elapsed_time);
+        printf("Time: %ld seconds, %ld nanoseconds | Value: %f\n",
+               elapsed_time.tv_sec - start_time.tv_sec,
+               elapsed_time.tv_nsec - start_time.tv_nsec,
+               ADS1256_GetChannalValue(1) * 5.0 / 0x7fffff);
 
-        printf("1 : %f\r\n", ADS1256_GetChannalValue(1) * 5.0 / 0x7fffff);
-        
-        nanosleep(&delay_time, NULL);  // 10マイクロ秒待つ
+        nanosleep(&delay_time, NULL); // 10マイクロ秒待つ
 
         clock_gettime(CLOCK_MONOTONIC, &current_time);
     }
